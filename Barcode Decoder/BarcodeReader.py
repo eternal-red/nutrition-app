a983fa181dd0608e2f5f11b57ee98f5abd75e27d
Yuri Xu <80001670+eternal-red@users.noreply.github.com>
2024-01-24 09:19:52 -0500

    revisions

--- Barcode Decoder/BarcodeReader.py
+++ Barcode Decoder/BarcodeReader.py
@@ -2,36 +2,32 @@
 import cv2
 from pyzbar.pyzbar import decode
   
-# Make one method to decode the barcode 
+ 
 def BarcodeReader(image):
-    # read the image in numpy array using cv2
     img = cv2.imread(image)    
-    # Decode the barcode image
     detectedBarcodes = decode(img)    
     # If not detected then print the message
     if not detectedBarcodes:
-        print("Barcode Not Detected or your barcode is blank/corrupted!")
+        print("Barcode Not Detected")
     else:    
           # Traverse through all the detected barcodes in image
         for barcode in detectedBarcodes:        
-            # Locate the barcode position in image
             (x, y, w, h) = barcode.rect        
-            # Put the rectangle in image using 
-            # cv2 to highlight the barcode
             cv2.rectangle(img, (x-10, y-10),
                           (x + w+10, y + h+10), 
-                          (255, 0, 0), 2)       
+                          (255, 0, 0), 2)
+            print(barcode.type)       
+            print(len(barcode.data))
             if barcode.data!="":         
-            # Print the barcode data
-                print(barcode.data)
-                print(barcode.type)
+                print(barcode.data, "\n")
                 return barcode.data             
-    #Display the image (for debugging)
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
  
-if __name__ == "__main__":
-  # Take the image from user
-    image="Img.jpg"
-    BarcodeReader(image)
+def getImage():
+  vid = cv2.VideoCapture(0) 
+  while(True): 
+      ret, frame = vid.read() 
+      cv2.imshow('frame', frame) 
+      if cv2.waitKey(1) & 0xFF == ord('q'): 
+          break
+  vid.release()
+  cv2.destroyAllWindows() 
--- Barcode Decoder/Main.py
+++ Barcode Decoder/Main.py
@@ -1,18 +1,22 @@
 import requests
-from BarcodeReader import BarcodeReader
+from BarcodeReader import BarcodeReader, getImage
 
 def main():
     apiKey="WvJcUpdiej9dX4GGDhFS6ceCzZxmwUg9SetWsqvt"
-    image="Img.jpg"
-    gtin=BarcodeReader(image)
+    image="IMG_3958.JPG"
+    gtin=str(BarcodeReader(image)) #casting from bytes to str
+    gtin=gtin[3:len(gtin)-1] #2 or 3??
+    print(gtin)
     #sample gtin
     #gtin=856579002927
     url=f"https://api.nal.usda.gov/fdc/v1/foods/search?query={gtin}&pageSize=10&api_key={apiKey}"
+    print("\n-----------",url,"\n-------------\n")
     #sample link
     #'''https://api.nal.usda.gov/fdc/v1/foods/search?query=856579002927&pageSize=10&api_key=WvJcUpdiej9dX4GGDhFS6ceCzZxmwUg9SetWsqvt'''
     response=requests.get(url)
-    print(response)
-    print(response.content)
+    data = response.json()
+    #print(response)
+    print(data["foods"][0]["description"])
     print("sucess") 
  
 #pandas and organizing  stuff blah blah blah
@@ -20,3 +24,5 @@ if __name__ == "__main__":
  main()
 
  
+# b'0052603041843'
+# b'0016000124790'
