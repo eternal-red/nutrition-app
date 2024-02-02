505539e6bf5b67cf0cbc93caa2dbfdb43d01e018
Yuri Xu <80001670+eternal-red@users.noreply.github.com>
2024-02-02 18:43:30 -0500

    fixed barcode scanner

--- Barcode Decoder/BarcodeReader.py
+++ Barcode Decoder/BarcodeReader.py
@@ -9,6 +9,7 @@ def BarcodeReader(image):
     # If not detected then print the message
     if not detectedBarcodes:
         print("Barcode Not Detected")
+        return 0
     else:    
           # Traverse through all the detected barcodes in image
         for barcode in detectedBarcodes:        
@@ -16,10 +17,10 @@ def BarcodeReader(image):
             cv2.rectangle(img, (x-10, y-10),
                           (x + w+10, y + h+10), 
                           (255, 0, 0), 2)
-            print(barcode.type)       
-            print(len(barcode.data))
+            #print(barcode.type)       
+            #print(len(barcode.data))
             if barcode.data!="":         
-                print(barcode.data, "\n")
+                #print(barcode.data, "\n")
                 return barcode.data             
  
 def getImage():
@@ -31,3 +32,27 @@ def getImage():
           break
   vid.release()
   cv2.destroyAllWindows() 
+
+def BarcodeScanner():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        if BarcodeReader(ret)!=0:
+            break
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
--- Barcode Decoder/Main.py
+++ Barcode Decoder/Main.py
@@ -3,26 +3,32 @@ from BarcodeReader import BarcodeReader, getImage
 
 def main():
     apiKey="WvJcUpdiej9dX4GGDhFS6ceCzZxmwUg9SetWsqvt"
-    image="IMG_3958.JPG"
+    image="IMG_4313.JPG"
     gtin=str(BarcodeReader(image)) #casting from bytes to str
-    gtin=gtin[3:len(gtin)-1] #2 or 3??
-    print(gtin)
-    #sample gtin
-    #gtin=856579002927
-    url=f"https://api.nal.usda.gov/fdc/v1/foods/search?query={gtin}&pageSize=10&api_key={apiKey}"
-    print("\n-----------",url,"\n-------------\n")
-    #sample link
-    #'''https://api.nal.usda.gov/fdc/v1/foods/search?query=856579002927&pageSize=10&api_key=WvJcUpdiej9dX4GGDhFS6ceCzZxmwUg9SetWsqvt'''
-    response=requests.get(url)
-    data = response.json()
-    #print(response)
-    print(data["foods"][0]["description"])
-    print("sucess") 
+    gtin=gtin[2:len(gtin)-1] #2 or 3?
+    while True:   
+        if len(gtin)<12:
+           print("food not in database")
+           break 
+        print(gtin)
+        url=f"https://api.nal.usda.gov/fdc/v1/foods/search?query={gtin}&pageSize=10&api_key={apiKey}"
+        print("\n-----------",url,"\n-------------\n")
+        #sample link
+        #'''https://api.nal.usda.gov/fdc/v1/foods/search?query=856579002927&pageSize=10&api_key=WvJcUpdiej9dX4GGDhFS6ceCzZxmwUg9SetWsqvt'''
+        response=requests.get(url)
+        data = response.json()
+        #print(response) 
+        print("\n-----------\n-------------\n")
+        if data["totalHits"]==1:
+            print(data["foods"][0]["description"])
+            break
+        else: 
+            print("error!")
+            gtin=gtin[1:len(gtin)-1]
+     
  
 #pandas and organizing  stuff blah blah blah
 if __name__ == "__main__":
  main()
 
- 
-# b'0052603041843'
-# b'0016000124790'
+  
