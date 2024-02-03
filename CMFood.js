096ffee5186e4d09b91178ff4e70706f8ef46b44
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2024-02-03 10:06:48 -0500

    Update CMFood.js

--- CMFood.js
+++ CMFood.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, collectionGroup, doc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+//import { getDatabase, ref, child, get } from "firebase/database";
+
 //import { getAnalytics, collection, collectionGroup, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,12 +26,45 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const dbRef = ref(getDatabase());
+let diningLocationSnapshot;
+let locationFoodItemsSnapshot;
 
-console.log('test');
+// Get a snapshot of all dining options within diningLocations from the firebase database
+get(child(dbRef, `diningLocations`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    diningLocationSnapshot = snapshot.val()
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+// temporary test
+console.log('Saturday');
+
+
+location = 'BuildPizza'
+// Get a snapshot of food items from a specific location from the firebase database
+get(child(dbRef, `diningLocations/${location}/locationFoodItems`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    locationFoodItemsSnapshot = snapshot.val
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+
+
 
 // Establish collection group for Dining Places, then collections for food items and their nutrition
-const diningLocationColl = collection(db, "diningLocations");
-const mealItemColl = collectionGroup(db, "locationFoodItems");
+//const diningLocationColl = collection(db, "diningLocations");
+//const mealItemColl = collectionGroup(db, "locationFoodItems");
 
 //This should later extract data from the database
 function findAllMeals() {
